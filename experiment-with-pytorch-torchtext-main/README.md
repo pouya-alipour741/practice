62d80909482f3cf62c79ec9571231085a090ce90
Pouya Alipour <pouya.alipour741@gmail.com>
2024-07-10 21:09:29 +0330

    updated gitignore remove heavy files and added more practice

---
+++ experiment-with-pytorch-torchtext-main/README.md
@@ -0,0 +1,10 @@
+# Experiment with `TorchText` and `PyTorch`, and made a Sentiment classification model
+
+I have tried to explore the `TorchText` library using kaggle dataset https://www.kaggle.com/datasets/jp797498e/twitter-entity-sentiment-analysis.
+
+I only focussed here to explore the different things, not on accuracy of the model. For that, I did not used full dataset and not performed the raw text processing here.
+
+Main code is written in `main.py` file and data processing code is written in `data_process.py` file. Main experiment was done in Jupyter notebook `rough_experiment.ipynb` file, and after that all codes written in organized manner in `VS code`.  
+
+### Another Repository Link
+I have made a proper `Sentiment Analysis Model` using PyTorch with `modular coding approach`, link is given here https://github.com/Kousik-Sasmal/tweeter-sentiment-using-pytorch-torchtext.
---
+++ experiment-with-pytorch-torchtext-main/data_process.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import zipfile
+import os
+
+# initializing `artifacts` folder
+os.makedirs('artifacts',exist_ok=True)
+
+
+with zipfile.ZipFile('data/archive.zip','r') as zip_ref:
+    zip_ref.extractall('data')
+
+# Loading the "train data"
+df = pd.read_csv('data/twitter_training.csv',header=None)
+df.dropna(inplace=True)
+df.drop_duplicates(inplace=True)
+
+# Rename the column with index 3 to "tweet"
+df = df.rename(columns={3: "tweet"})
+
+# Convert the column with index 2 to a categorical data type and assign it to a new column called "sentiment"
+df['sentiment'] = df[2].astype('category')
+
+# Add a numerical label to the sentiment column using pandas' categorical codes
+df["label"] = df["sentiment"].cat.codes
+
+# Save selected columns of the dataframe to a csv file
+train_cleaned_path = "artifacts/train_cleaned.csv"
+
+df[['tweet', 'sentiment', 'label']].to_csv(train_cleaned_path, index=False)
+
+
+#---------------------------------------------------------------------------#
+
+
+# Loading the "validation data"
+df_valid = pd.read_csv('data/twitter_validation.csv',header=None)
+df_valid.dropna(inplace=True)
+df_valid.drop_duplicates(inplace=True)
+
+# Rename the column with index 3 to "tweet"
+df_valid = df_valid.rename(columns={3: "tweet"})
+
+# Convert the column with index 2 to a categorical data type and assign it to a new column called "sentiment"
+df_valid['sentiment'] = df_valid[2].astype('category')
+
+# Add a numerical label to the sentiment column using pandas' categorical codes
+df_valid["label"] = df_valid["sentiment"].cat.codes
+
+# Save selected columns of the dataframe to a csv file
+valid_cleaned_path = "artifacts/valid_cleaned.csv"
+
+df_valid[['tweet', 'sentiment', 'label']].to_csv(valid_cleaned_path, index=False)
